10d3c6a4c9078518ee8c7885ea961918bc6cd15f
Sergey Toyliev <doyalnitay@gmail.com>
2018-11-09 16:36:32 +0500

    Add test with Catch2

--- Algoritms_1_BubbleSort.cpp
+++ Algoritms_1_BubbleSort.cpp
@@ -1,43 +1,39 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 using namespace std;
 
-template<typename T>
-void BubbleSort(vector<T> &vec) {
-    for (size_t i = 0; i < vec.size(); ++i) {
-        bool check = false;
-        for (size_t j = 0; j < vec.size() - i - 1; ++j) {
-            if (vec[j] > vec[j + 1]) {
-                T temp = vec[j + 1];
-                vec[j + 1] = vec[j];
-                vec[j] = temp;
-                check = true;
-            }
-        }
-        if (!check) {
-            break;
-        }
-    }
+template<typename T> void BubbleSort(vector<T> &vec) {	
+	for (size_t i = 0; i < vec.size(); ++i) {
+		bool check = false;
+		for (size_t j = 0; j < vec.size() - i - 1; ++j) {
+			if (vec[j] > vec[j + 1]) {
+				T temp = vec[j + 1];
+				vec[j + 1] = vec[j];
+				vec[j] = temp;
+				check = true;
+			}
+		}
+		if (!check) {
+			break;
+		}
+	}
 }
 
-template<typename T>
-void PrintVector(vector<T> &vec) {
-    for (auto item : vec) {
-        cout << item << " ";
-    }
-    cout << endl;
+TEST_CASE("Sorting integers") {
+	vector<int> collection_unsorted = {1, 10, 2, 5, 6, 3, 9, 4, 8, 7, 0};
+	vector<int> collection_sorted = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+	BubbleSort(collection_unsorted);
+		
+	REQUIRE(collection_sorted == collection_unsorted);
 }
 
-int main() {
-    vector<int> vec_int = {1, 10, 2, 5, 6, 3, 9, 4, 8, 7, 0};
-    BubbleSort(vec_int);
-    PrintVector(vec_int);
-
-    vector<string> vec_str = {"bb", "ccc", "aaa", "b", "a"};
-    BubbleSort(vec_str);
-    PrintVector(vec_str);
-
-    return 0;
+TEST_CASE("Sorting string") {
+	vector<string> collection_unsorted = {"bb", "ccc", "aaa", "b", "a"};
+	vector<string> collection_sorted = {"a", "aaa", "b", "bb", "ccc"};
+	
+	BubbleSort(collection_unsorted);
+	
+    REQUIRE(collection_sorted == collection_unsorted);
 }
