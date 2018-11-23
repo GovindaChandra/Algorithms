a62d4003c46621925ccaa081bdf54adf2d476d18
Sergey Toyliev <doyalnitay@gmail.com>
2018-11-23 17:24:16 +0500

    Formating code

--- Algoritms_2_SelectSort.cpp
+++ Algoritms_2_SelectSort.cpp
@@ -26,15 +26,15 @@ void SelectSort(vector <T> &vec) {
 }
 
 TEST_CASE("Sorting integers") {
-		vector<int> collection_unsorted = {1, 10, 2, 5, 6, 3, 9, 4, 8, 7, 0};
-		vector<int> collection_sorted = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		SelectSort(collection_unsorted);
-		REQUIRE(collection_sorted == collection_unsorted);
-	}
+	vector<int> collection_unsorted = {1, 10, 2, 5, 6, 3, 9, 4, 8, 7, 0};
+	vector<int> collection_sorted = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	SelectSort(collection_unsorted);
+	REQUIRE(collection_sorted == collection_unsorted);
+}
 
 TEST_CASE("Sorting string") {
 	vector<string> collection_unsorted = {"bb", "ccc", "aaa", "b", "a"};
 	vector<string> collection_sorted = {"a", "aaa", "b", "bb", "ccc"};
 	SelectSort(collection_unsorted);
-    REQUIRE(collection_sorted == collection_unsorted);
+	REQUIRE(collection_sorted == collection_unsorted);
 }
